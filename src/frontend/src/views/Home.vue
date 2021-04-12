f04c5e72571f1413607a35db5a018d5a64ed57b5
dabin8024 <dabin8024@naver.com>
2021-04-12 15:26:43 +0900

    add file about vue

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Test msg="GWelcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Test from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Test
   }
 };
 </script>
